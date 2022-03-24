fd3409189cf2fc2dd78cf5d3010145e70e015c48
Tsenda-Djessy-Flavien <djesstsenda@gmail.com>
2022-03-24 15:17:35 +0100

    complete location app flutter

---
+++ android/app/src/main/kotlin/com/example/traking/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.traking
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/secrets.dart
@@ -0,0 +1,4 @@
+class Secrets {
+  // Add your Google Maps API Key here
+  static const API_KEY = 'AIzaSyC_ooMDgy5CMmUEgd2759JdFv-igK3f6qE';
+}
